4567beecfa53c76283302b32338f9a9106b27d49
fujiokayu <fujiokayu@gmail.com>
2021-07-04 15:47:45 +0900

    change values api to simple one

--- aspnet-api-container/Controllers/ValuesController.cs
+++ aspnet-api-container/Controllers/ValuesController.cs
@@ -9,31 +9,10 @@ namespace aspnet_api_container.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        // GET api/values/
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            return System.Guid.NewGuid().ToString();
         }
     }
 }
